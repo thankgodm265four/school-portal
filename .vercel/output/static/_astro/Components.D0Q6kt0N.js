06778085c87c6c93a4fe308303db677c362d8819
Administrator <admin@schoolportal.local>
2026-01-12 07:44:10 -0800

    Complete authentication system: student login with firstName + matricNumber, teacher login with firstName + staffId, admin credential management

---
+++ .vercel/output/static/_astro/Components.D0Q6kt0N.js
@@ -0,0 +1,1 @@
+import{T as m}from"./Components.gAA2fnHL.js";import"./jsx-runtime.D_zvdyIk.js";import"./index.JXKNaeUN.js";export{m as TiltCard};
--- .vercel/output/static/_astro/Components.DF0gmBdW.js
+++
@@ -1,1 +0,0 @@
-import{T as m}from"./Components.R871H6Pk.js";import"./jsx-runtime.D_zvdyIk.js";import"./index.JXKNaeUN.js";export{m as TiltCard};
